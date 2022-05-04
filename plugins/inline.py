49cb8ebb17d83114aeba2985beb54ac2d29c6ffc
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-05-04 13:02:48 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -103,7 +103,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('Search again', @DTG_Filter_bot=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
