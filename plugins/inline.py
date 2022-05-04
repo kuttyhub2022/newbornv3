79dfc7d1d55f57e418728dd057f806c74345d2dc
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-05-04 18:53:52 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -6,7 +6,7 @@ from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 
-logger = logging.getLogger(name)
+logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
 
 async def inline_users(query: InlineQuery):
