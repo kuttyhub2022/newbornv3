1db2bb17a0bab285636d86bff248d7a7045a4590
kuttyhub2022 <106232707+kuttyhub2022@users.noreply.github.com>
2022-06-25 08:41:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -404,15 +404,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-        ], [
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/Is_Movie_Time')
         ], [
             InlineKeyboardButton('🌟 Main Channel', url='https://t.me/Is_Movie_Time'),
             InlineKeyboardButton('💥 Bot Movie Channel', url='https://t.me/Is_Movie_Time')
         ], [
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
